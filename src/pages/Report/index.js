42580666d52537b46fba1e9bd344b47d18cc7991
celso <celsosr87@gmail.com>
2020-07-04 15:21:52 -0300

    Tela de Relatório

---
+++ src/pages/Report/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, StyleSheet, Picker, Button} from 'react-native';
+
+import BalanceLabel from '../../components/BalanceLabel';
+import EntrySummary from '../../components/EntrySummary';
+import EntryList from '../../components/EntryList';
+
+const Report = () => {
+  return (
+    <View>
+      <BalanceLabel />
+      <View>
+        <Picker>
+          <Picker.Item label="Todas Categorias" />
+        </Picker>
+        <Picker>
+          <Picker.Item label="Últimos 7 dias" />
+        </Picker>
+      </View>
+      <EntrySummary />
+      <EntryList />
+      <View>
+        <Button title="Salvar" />
+        <Button title="Fechar" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+  },
+});
+
+export default Report;
